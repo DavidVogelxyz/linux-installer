a10be6223975a891a45c327b30ea9d4fe8a01e62
David Vogel <david@davidvogel.xyz>
2025-03-09 01:24:54 -0700

    recovered from Ubuntu VM fail, and made additional progress
    this commit includes 99% of the progress made before the Ubuntu VM fail. in addition, this commit includes further progress made

---
+++ lib.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+################################
+# VARIABLES
+################################
+
+os_supported=(
+    "arch"
+    "artix"
+    "debian"
+    "ubuntu"
+)
+
+################################
+# FUNCTIONS
+################################
+
+#error() {
+#
+#}
+
+get_os() {
+    for os in "${os_supported[@]}"; do
+        grep "ID=$os" /etc/os-release > /dev/null 2>&1 \
+            && export setup_os="$os" \
+            && break
+    done
+}
+
+get_uefi() {
+    uefi="0"
+
+    # UEFI check #1
+    mount | grep efi > /dev/null 2>&1 \
+        && uefi="1"
+
+    # UEFI check #2
+    ls /sys/firmware/efi > /dev/null 2>&1 \
+        && uefi="1"
+}
+
+get_disks() {
+    # store the `lsblk` output into an array
+    mapfile -t list_disks < <(lsblk | grep disk | awk '{print $1}')
+
+    # how many disks?
+    disk_count=${#list_disks[@]}
+
+    # instantiate `disk_selected`
+    disk_selected="not selected yet"
+
+    # if `disk_count` is 1, set it as the disk selected without asking the user
+    [[ $disk_count == "1" ]] \
+        && disk_selected="${list_disks[0]}"
+}
+
+set_partition_names() {
+    # could turn this into a loop...
+    partition_boot_sda1="${disk_selected}1"
+    partition_boot_sda2="${disk_selected}2"
+    partition_boot_sda3="${disk_selected}3"
+    partition_boot_sda4="${disk_selected}4"
+    partition_boot_sda5="${disk_selected}5"
+}
+
+get_ram_size() {
+    # `--si` gets the human readable `-h` in units of "GB"
+    ram_size=$(free -th --si | grep "Total" | awk '{print $2}')
+}
+
+check_so_far() {
+    echo -e "\nSTATUS:"
+    echo "OS = $setup_os"
+
+    echo -e "\nDISKS:"
+    echo "DISK COUNT = $disk_count"
+    echo "DISKS = ${list_disks[@]}"
+    echo "DISK SELECTED = $disk_selected"
+
+    echo -e "\nOTHER:"
+    echo "UEFI? = $uefi"
+    echo "Partition 4 = $partition_boot_sda4"
+    echo "RAM: $ram_size"
+}
---
+++ main.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#set -x
+
+# source the commands
+source lib.sh
+
+get_os # sets `setup_os`
+
+get_uefi # sets `uefi` (0 is "legacy BIOS")
+
+get_disks # sets `disk_count`, returns a selected disk only if `disk_count` is 1
+
+set_partition_names
+
+get_ram_size # set `ram_size` in the format of `xyGB`
+
+check_so_far # checks progress
+
+################################
+# Pre-chroot
+################################
+
+#get_os
+
+#get_uefi
+
+#get_disks
+
+################################
+# Post-chroot
+################################
+
+#get_os
